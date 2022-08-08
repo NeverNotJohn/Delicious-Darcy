07338d6fc8e8a75ae8cc984096e24ea4f7b6de99
John <110793201+NeverNotJohn@users.noreply.github.com>
2022-08-07 20:52:04 -0700

    Add files via upload
    Delicious Darcy mk1, new and more organized. Previously worked on.

---
+++ func.py
@@ -0,0 +1,9 @@
+" function defintion: "
+import discord
+from discord.ext import commands
+from asyncio import sleep
+import atexit
+from dd import client
+from dd import bot
+
+" accessing functions: "

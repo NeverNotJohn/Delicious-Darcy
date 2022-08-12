15450602e3bc0566f76889b33b9f7f850eb8a351
John Herrera <johntheherrera@gmail.com>
2022-08-12 13:35:36 -0700

    as

--- func.py
+++ func.py
@@ -1,13 +1,23 @@
 " GLOBAL VARIABLES "
 userNum = 0 # FIXME
+runTime = 0
 
 " Creation Functions "
+
+def create(user):
+    print("hi")
 
 " accessing functions: "
 
 # Number of current users being observed
 def NUM():
     return userNum
+
+def checkTime(user): # FIXME
+    return "returns time spent on discord from user"
+
+def checkUser(user): # FIXME
+    return "Returns true if user is not on database"
 
 " Memory Accessors "
 
@@ -16,7 +26,7 @@ MAIN FUNCTIONS
 """
 
 def observe(user): # FIXME
-    return 0
+    print("starts tracking discord users")
 
 def stopObserve(user): # FIXME 
-    return 0
+    print("stops observing... add to total time on")

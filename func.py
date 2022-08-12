2f162f69b0372fb403c8fd3578d96fd3acecc1b0
John Herrera <johntheherrera@gmail.com>
2022-08-12 13:28:11 -0700

    a

--- func.py
+++ func.py
@@ -1,10 +1,22 @@
 " GLOBAL VARIABLES "
 userNum = 0 # FIXME
 
+" Creation Functions "
 
 " accessing functions: "
 
 # Number of current users being observed
-
 def NUM():
     return userNum
+
+" Memory Accessors "
+
+"""
+MAIN FUNCTIONS
+"""
+
+def observe(user): # FIXME
+    return 0
+
+def stopObserve(user): # FIXME 
+    return 0
